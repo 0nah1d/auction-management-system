d517d81fad6ff510fd6442e615d901f9abbef650
Nahid <nahidthedeveloper@gmail.com>
2024-12-01 13:51:10 +0600

    add some models

--- auctions/models.py
+++ auctions/models.py
@@ -94,6 +94,8 @@ class Comments(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     listing = models.ForeignKey(AuctionList, on_delete=models.CASCADE)
     comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"Comment by {self.user.username} on {self.listing.title}"
